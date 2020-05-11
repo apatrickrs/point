2b98b412a38f8faadf07c0c5ab1f5583759a6054
Andrew Patrick <andrew.prsilva@gmail.com>
2020-05-11 20:35:57 -0300

    Project created

---
+++ src/main/java/com/andrew/point/MainFrame.java
@@ -0,0 +1,13 @@
+package com.andrew.point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainFrame {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainFrame.class, args);
+	}
+
+}
